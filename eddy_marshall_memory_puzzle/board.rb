78e390a9e2e0e6c163d0196a068720aa5645651b
Minh Nguyen <m9nguyen@ucsd.edu>
2021-06-29 11:24:27 -0700

    second swap

--- eddy_marshall_memory_puzzle/board.rb
+++ eddy_marshall_memory_puzzle/board.rb
@@ -1,11 +1,20 @@
 class Board
 
-    def
-
-
-
-    def ==(object)
-        
+    def initialize
+        @board = Array.new(4) {Array.new(4,nil)}
     end
+
+    def populate
+        count = 0
+        while count < @board.length 
+        @card_1 = Card.new
+        @card_2 = @card_1
+        row = rand(0..3) 
+        col = rand(0..3)
+        @board[row][col] = @card_1.value if @board[row][col] == nil
+
+    end
+#hello
+
 
 end
